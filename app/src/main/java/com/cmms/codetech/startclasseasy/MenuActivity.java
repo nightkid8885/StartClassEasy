0bdd893397a53c4088505a93be3afe3f7f59eec9
Estelle <estelleccl@gmail.com>
2015-10-03 17:15:45 +0800

    first commit

--- app/src/main/java/com/cmms/codetech/startclasseasy/MenuActivity.java
+++ app/src/main/java/com/cmms/codetech/startclasseasy/MenuActivity.java
@@ -5,20 +5,18 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class MenuActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-//        setRequestedOrientation(.SCREEN_ORIENTATION_PORTRAIT);
+        setContentView(R.layout.activity_menu);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_menu, menu);
         return true;
     }
 
