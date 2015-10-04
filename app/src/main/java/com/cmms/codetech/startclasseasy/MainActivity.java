5795249498c95d9133ccd63cfdfc561935f7b823
Estelle <estelleccl@gmail.com>
2015-10-04 13:11:27 +0800

    Merge branch 'master' of https://github.com/nightkid8885/StartClassEasy into estelle
    # Conflicts:
    #	app/src/main/AndroidManifest.xml
    #	app/src/main/res/values/strings.xml
    
    Login Page & Menu Layout Done

--- app/src/main/java/com/cmms/codetech/startclasseasy/MainActivity.java
+++ app/src/main/java/com/cmms/codetech/startclasseasy/MainActivity.java
@@ -1,38 +1,46 @@
 package com.cmms.codetech.startclasseasy;
 
+import android.content.Intent;
+import android.preference.EditTextPreference;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
+
+    Button main_submitBtn;
+    EditText main_email, main_password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//        findViewById(R.id.am_submitBtn).setOnClickListener((View.OnClickListener) this);
+
+        main_email = (EditText) findViewById(R.id.main_email);
+        main_password = (EditText) findViewById(R.id.main_password);
+        main_submitBtn = (Button) findViewById(R.id.main_submitBtn);
 
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    public void onSignIn(View button){ //behaviour
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        if(main_email.getEditableText().toString().contentEquals("abc@abc.com") &&
+                main_password.getEditableText().toString().contentEquals("password")) {
+
+
+            Intent i = new Intent(this, MenuActivity.class);
+            startActivity(i);
+        }
+        else {
+            Toast.makeText(this, "Invalid username or password, please try again", Toast.LENGTH_LONG).show();
         }
 
-        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/com/cmms/codetech/startclasseasy/MenuActivity.java
+++ app/src/main/java/com/cmms/codetech/startclasseasy/MenuActivity.java
@@ -1,37 +1,49 @@
 package com.cmms.codetech.startclasseasy;
 
+import android.app.Activity;
+import android.app.ListActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
-public class MenuActivity extends AppCompatActivity {
+public class MenuActivity extends ListActivity {
+
+    ListView list;
+    String[] itemname ={
+            "Calendar",
+            "Feedback",
+            "Admins"
+
+    };
+    Integer[] imgid={
+            R.drawable.pic1,
+            R.drawable.pic2,
+            R.drawable.pic3
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
-    }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_menu, menu);
-        return true;
-    }
+        MenuItemAdapter adapter=new MenuItemAdapter(this, itemname, imgid);
+//        list=(ListView)findViewById(R.id.list_item);
+        list=(ListView)findViewById(android.R.id.list);
+        list.setAdapter(adapter);
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id){
+                String Slecteditem= itemname[+position];
+                Toast.makeText(getApplicationContext(), Slecteditem, Toast.LENGTH_SHORT).show();
+            }
+        });
         }
 
-        return super.onOptionsItemSelected(item);
-    }
 }
