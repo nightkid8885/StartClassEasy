fddd070436ee92976a7f10b17d35ab797a93cb30
nightkid8885 <onnchai85@gmail.com>
2015-10-04 14:14:35 -0700

    daryl 20151004 0213pm

--- app/src/main/java/com/cmms/codetech/startclasseasy/AdminActivity.java
+++ app/src/main/java/com/cmms/codetech/startclasseasy/AdminActivity.java
@@ -1,17 +1,49 @@
 package com.cmms.codetech.startclasseasy;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.view.View.OnClickListener;
 
 public class AdminActivity extends AppCompatActivity {
+
+    EditText searchCourseEt;
+    Button addNewCourseBtn;
+    ListView listViewLv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin);
+
+        initView();
+
+        addNewCourseBtn.setOnClickListener(newCourse);
+
+
+
     }
+
+    private void initView() {
+        searchCourseEt = (EditText) findViewById(R.id.aa_searchCourseEt);
+        addNewCourseBtn = (Button) findViewById(R.id.aa_newBtn);
+        listViewLv = (ListView) findViewById(R.id.aa_listviewLv);
+    }
+
+    OnClickListener newCourse = new OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            startActivity(new Intent(AdminActivity.this,
+                    CourseActivity.class));
+        }
+    };
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
--- app/src/main/java/com/cmms/codetech/startclasseasy/CourseActivity.java
+++ app/src/main/java/com/cmms/codetech/startclasseasy/CourseActivity.java
@@ -1,17 +1,114 @@
 package com.cmms.codetech.startclasseasy;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.view.View.OnClickListener;
+
+import java.util.Calendar;
 
 public class CourseActivity extends AppCompatActivity {
+
+    EditText courseNameEt;
+    EditText conductorNameEt;
+    Button courseDateBtn;
+    ListView ac_courseDateLv;
+
+    private TextView Output;
+
+    private int year;
+    private int month;
+    private int day;
+
+    static final int DATE_PICKER_ID = 1111;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_course);
+
+        initView();
+
+        //http://androidexample.com/In_this_example_creating_a_date_picker_to_pick_day__month_year_of_date/index.php?view=article_discription&aid=89&aaid=113
+        // Get current date by calender
+
+        final Calendar c = Calendar.getInstance();
+        year  = c.get(Calendar.YEAR);
+        month = c.get(Calendar.MONTH);
+        day   = c.get(Calendar.DAY_OF_MONTH);
+
+        // Show current date
+
+        Output.setText(new StringBuilder()
+                // Month is 0 based, just add 1
+                .append(month + 1).append("-").append(day).append("-")
+                .append(year).append(" "));
+
+        // Button listener to show date picker dialog
+
+        courseDateBtn.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                // On button click show datepicker dialog
+                showDialog(DATE_PICKER_ID);
+
+            }
+
+        });
+
     }
+
+    private void initView() {
+        courseNameEt = (EditText) findViewById(R.id.ac_courseNameEt);
+        conductorNameEt = (EditText) findViewById(R.id.ac_conductorNameEt);
+        courseDateBtn = (Button) findViewById(R.id.ac_courseDateBtn);
+        ac_courseDateLv = (ListView) findViewById(R.id.ac_courseDateLv);
+
+        Output = (TextView) findViewById(R.id.ac_conductorNameTv);
+    }
+
+    @Override
+    protected Dialog onCreateDialog(int id) {
+        switch (id) {
+            case DATE_PICKER_ID:
+
+                // open datepicker dialog.
+                // set date picker for current date
+                // add pickerListener listner to date picker
+                return new DatePickerDialog(this, pickerListener, year, month,day);
+        }
+        return null;
+    }
+
+    private DatePickerDialog.OnDateSetListener pickerListener = new DatePickerDialog.OnDateSetListener() {
+
+        // when dialog box is closed, below method will be called.
+        @Override
+        public void onDateSet(DatePicker view, int selectedYear,
+                              int selectedMonth, int selectedDay) {
+
+            year  = selectedYear;
+            month = selectedMonth;
+            day   = selectedDay;
+
+            // Show selected date
+            Output.setText(new StringBuilder().append(month + 1)
+                    .append("-").append(day).append("-").append(year)
+                    .append(" "));
+
+        }
+    };
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
