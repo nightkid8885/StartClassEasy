b30a6a141f78563899e277b43a8d92964e13cf81
nightkid8885 <onnchai85@gmail.com>
2015-10-25 13:22:07 -0700

    Merge branch 'master' of https://github.com/nightkid8885/StartClassEasy
    # Conflicts:
    #	app/app.iml

---
+++ app/src/main/java/com/cmms/codetech/startclasseasy/adapter/CourseDateAdapter.java
@@ -0,0 +1,90 @@
+package com.cmms.codetech.startclasseasy.adapter;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.cmms.codetech.startclasseasy.R;
+import com.cmms.codetech.startclasseasy.model.CourseDate;
+
+import java.util.List;
+
+/**
+ * Created by daryl on 24/10/2015.
+ */
+public class CourseDateAdapter  extends ArrayAdapter<CourseDate> {
+
+    private List<CourseDate> courseDateList;
+    private Context context;
+
+    public CourseDateAdapter(Context ctx, List<CourseDate> courseDateList) {
+        super(ctx, R.layout.activity_course_row_date, courseDateList);
+        this.courseDateList = courseDateList;
+        this.context = ctx;
+    }
+
+    @Override
+    public int getCount() {
+        return super.getCount();
+    }
+
+    @Override
+    public int getPosition(CourseDate item) {
+        return super.getPosition(item);
+    }
+
+    @Override
+    public CourseDate getItem(int position) {
+        return super.getItem(position);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View v = convertView;
+        CourseDateHolder holder = new CourseDateHolder();
+
+        if (convertView == null) {
+
+            Log.e("1", "1");
+
+            LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            v = inflater.inflate(R.layout.activity_course_row_date, null);
+
+            TextView courseDate = (TextView) v.findViewById(R.id.acrd_courseDateTimeTv);
+            ImageView minus = (ImageView) v.findViewById(R.id.acrd_removeDateTimeIv);
+
+            minus.setTag(position);
+            holder.courseDate = courseDate;
+            holder.minus = minus;
+            v.setTag(holder);
+        } else {
+
+            holder = (CourseDateHolder) v.getTag();
+        }
+        CourseDate p = courseDateList.get(position);
+        holder.courseDate.setText(p.getCourseDate());
+        holder.minus.setOnClickListener(minus);
+
+        return v;
+    }
+
+    private static class CourseDateHolder {
+        public TextView courseDate;
+        public ImageView minus;
+
+        // public ImageView image;
+    }
+
+    View.OnClickListener minus = new View.OnClickListener() {
+
+        @Override
+        public void onClick(View v) {
+        }
+    };
+}
